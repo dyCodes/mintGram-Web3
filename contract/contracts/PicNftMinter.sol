0766f5d7859568579c654932543030478fcbd701
dyCodes <yusufdaudu51@gmail.com>
2024-06-16 09:20:32 +0100

    feat: update contract - add count & getAllNFTs

--- contract/contracts/PicNftMinter.sol
+++ contract/contracts/PicNftMinter.sol
@@ -8,6 +8,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract PicNftMinter is ERC721, ERC721URIStorage, Ownable {
     uint256 private _nextTokenId;
+    mapping(string => uint8) existingURIs;
 
     constructor(
         address initialOwner
@@ -18,8 +19,6 @@ contract PicNftMinter is ERC721, ERC721URIStorage, Ownable {
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
     }
-
-    // The following functions are overrides required by Solidity.
 
     function tokenURI(
         uint256 tokenId
@@ -37,10 +36,35 @@ contract PicNftMinter is ERC721, ERC721URIStorage, Ownable {
         address recipient,
         string memory metadataURI
     ) public payable returns (uint256) {
+        require(existingURIs[metadataURI] != 1, "NFT already minted!");
+
         uint256 tokenId = _nextTokenId++;
+        existingURIs[metadataURI] = 1;
 
         _mint(recipient, tokenId);
         _setTokenURI(tokenId, metadataURI);
+
         return tokenId;
+    }
+
+    function count() public view returns (uint256) {
+        return _nextTokenId;
+    }
+
+    function getAllNFTs()
+        public
+        view
+        returns (uint256[] memory, string[] memory)
+    {
+        uint256 tokenCount = _nextTokenId;
+        uint256[] memory tokenIds = new uint256[](tokenCount);
+        string[] memory uris = new string[](tokenCount);
+
+        for (uint256 i = 0; i < tokenCount; i++) {
+            tokenIds[i] = i;
+            uris[i] = tokenURI(i);
+        }
+
+        return (tokenIds, uris);
     }
 }
